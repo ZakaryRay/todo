8737d2293eb91b2afec61955605e836172b58b65
Zakary Raymond <zakary-raymond@live.fr>
2025-10-20 22:56:00 -0400

    Add page is cleaner

--- frontend/src/Pages/Add.jsx
+++ frontend/src/Pages/Add.jsx
@@ -1,22 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import HomeButton from "../Components/HomeButton";
 
 const Add = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <div className="p-10">
-            <form className="bg-blue-500 p-10 mx-auto w-200">
-                <div className="flex flex-col">
-                    <label htmlFor="nameOfTask">What you need to do?</label>
-                    <input className="bg-blue-600 rounded-xl outline-0 p-1" type="text" />
+        <div className="p-10 bg-gradient-to-b from-[#D9D9D9] to-[#ADADAD]">
+            <HomeButton />
+            <form className=" p-10 mx-auto flex flex-col max-w-[800px] min-w-[300px] ">
+                <div className="flex flex-col mt-10">
+                    <label htmlFor="nameOfTask" className="m-auto thin text-4xl mb-4     ">
+                        Title
+                    </label>
+                    <input
+                        className="bg-gray-100 rounded-xl outline-0 p-4 inset-shadow-sm placeholder:text-center placeholder:text-2xl text-2xl"
+                        type="text"
+                        placeholder="Enter the title of the task"
+                    />
                 </div>
-                <div className="flex flex-col">
-                    <label htmlFor="descOfTask">Add details</label>
+                <div className="flex flex-col mt-30">
+                    <label htmlFor="descOfTask" className="m-auto thin text-4xl my-4">
+                        Description
+                    </label>
 
                     <textarea
-                        className="bg-blue-600 rounded-xl outline-0 p-1"
+                        className="bg-gray-100 h-[200px] rounded-xl outline-0 p-4 text-2xl inset-shadow-sm placeholder:text-center placeholder:text-2xl "
                         name="desc"
                         id="desc"
-                        placeholder="150 character max."></textarea>
+                        placeholder="Describe your task"></textarea>
                 </div>
+                <button
+                    onClick={handleSubmit}
+                    className="px-8 py-2 shadow-lg text-2xl mt-30 w-fit self-center bg-gradient-to-r rounded-xl  from-[#FFCC00] to-[#E29715] active:scale-95 text-white thin">
+                    Add task
+                </button>
             </form>
         </div>
     );
